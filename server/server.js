0d437c9b92363febf147ae7fca6e84570546a307
Imtiyaz Mohammed <imtiyazmohammed@Imtiyazs-MacBook-Pro.local>
2018-07-29 17:02:58 -0700

    added the new repo

--- server/server.js
+++ server/server.js
@@ -3,10 +3,10 @@ const express = require('express');
 const app = express();
 
 const publicPath = path.join(__dirname, '../public/');
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
 app.listen(PORT,()=>{
-    console.log('connected to app succesfully')
+    console.log('connected to app succesfully on some port')
 });
